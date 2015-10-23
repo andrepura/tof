7b76284b5e1d12d175013625d9836ed612e1ea38
andrepura <andre.pura@mappau.com>
2015-10-23 22:44:15 +0200

    showdata -> semi final

--- WebContent/js/showdata.js
+++ WebContent/js/showdata.js
@@ -1,87 +1,89 @@
 console.log("i m alive :) ");
 
+var MOCKING = false;
 
-function init(){
+function init() {
 	loadData();
-	//bind search
-	$("#filterTable-input").bind("propertychange change click keyup input paste", function(event){
-	    
-	    console.log("asd");
-	 });
+	// bind search
+	$("#filterTable-input").bind(
+			"propertychange change click keyup input paste", function(event) {
+				var query = $("#filterTable-input").val();
+				if (query.length > 2) {
+					console.log("Start search: " + query);
+					search(query);
+				} else {
+					console.log("toooooooooo short");
+				}
+			});
 }
 
+function loadData() {
+	console.log("loadData: MOCKING ?? "+MOCKING);
 
-function loadData(){
-	console.log("loadData ");
-	
-	//TODO: to use real data -> comment
-	var text ='[{"uid":1234,"id":"asdasd123456","firstname":"max","lastname":"mustermann","phone":"+436761231234","mail":"asd@sdf.com","age":20,"days":5,"section":"infra","test":null},{"uid":1235,"id":"asdasd123457","firstname":"maxima","lastname":"musterfrau","phone":"+436761231234","mail":"asd@sdasedf.com","age":42,"days":4,"section":"team"}]';
-	var obj = JSON.parse(text);
-	drawTable(obj);
-	return;
-	
-	$.ajax({
-	    url: '../mock/showdata_mock.json',
-	    type: "get",
-	    dataType: "json",
-	    
-	    success: function(data, textStatus, jqXHR) {
-	    	console.log("data --> "+data);
-	        // since we are using jQuery, you don't need to parse response
-	        drawTable(data);
-	    },
-	    error: function(error){
-	    	console.log("error --> ");
-	    	console.log(error);
-	    }	
-	});	
+	if (MOCKING) {
+		var text = '[{"uid":1234,"id":"asdasd123456","firstname":"max","lastname":"mustermann","phone":"+436761231234","mail":"asd@sdf.com","age":20,"days":5,"section":"infra","test":null},{"uid":1235,"id":"asdasd123457","firstname":"maxima","lastname":"musterfrau","phone":"+436761231234","mail":"asd@sdasedf.com","age":42,"days":4,"section":"team"}]';
+		var obj = JSON.parse(text);
+		drawTable(obj);
+		return;
+	}
 }
 
-function search(query){
-	console.log("Search for: "+query);
+function search(query) {
+	console.log("Search for: " + query);
 	clearData();
-	 $.ajax({
-         beforeSend: function() { $.mobile.showPageLoadingMsg(); }, //Show spinner
-         complete: function() { $.mobile.hidePageLoadingMsg() }, //Hide spinner
-         url: '../tof/search/'+encodeURI(query),
-         dataType: 'json',
-         success: function(data) {
-        	 console.log("");
-             console.log(data);
-         }
-     });
+	if (MOCKING) {
+		console.log("MOCKING!!!!!");
+		return;
+	}
+	$.ajax({
+		beforeSend : function() {
+			$.mobile.showPageLoadingMsg();
+		}, // Show spinner
+		complete : function() {
+			$.mobile.hidePageLoadingMsg()
+		}, // Hide spinner
+		url : '../tof/search?q=' + encodeURI(query),
+		dataType : 'json',
+		success : function(data) {
+			console.log("data:");
+			console.log(data);
+			drawTable(data);
+		}
+	});
 }
 
 function drawTable(data) {
 	console.log("loadData done");
-	if(data.length > 0){
+	if (data.length > 0) {
 		drawRowHeaders(data[0]);
 	}
-    for (var i = 0; i < data.length; i++) {
-        drawRow(data[i]);
-    }
+	for (var i = 0; i < data.length; i++) {
+		drawRow(data[i]);
+	}
 }
 
-function clearData(){
-	$("#show-data-table-header").clear();
-	$("#show-data-table").clear();
+function clearData() {
+	$("#show-data-table-header").empty();
+	$("#show-data-table").empty();
 }
 
-function drawRowHeaders(rowData){
+function drawRowHeaders(rowData) {
 	var row = $("<tr />")
-    $("#show-data-table").append(row);
-	
+	$("#show-data-table").append(row);
+
 	var keys = Object.getOwnPropertyNames(rowData);
 	console.log(keys);
-	for(var i in keys){
+	for ( var i in keys) {
 		row.append($("<td>" + keys[i] + "</td>"));
-	}	
+	}
 }
 
 function drawRow(rowData) {
-    var row = $("<tr />")
-    $("#show-data-table").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
-    for(var i in rowData){
+	var row = $("<tr />")
+	$("#show-data-table").append(row); // this will append tr element to
+	// table... keep its reference for a
+	// while since we will add cels into it
+	for ( var i in rowData) {
 		row.append($("<td>" + rowData[i] + "</td>"));
 	}
 }
